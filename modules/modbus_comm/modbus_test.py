1c19610ecbb7d518a896817e4be1d955d7070902
qqq000feel <qqq000feel@naver.com>
2020-08-25 16:16:53 +0900

    20200825

--- modules/modbus_comm/modbus_test.py
+++ modules/modbus_comm/modbus_test.py
@@ -1,11 +1,17 @@
 from easymodbus.modbusClient import ModbusClient
 from easymodbus.modbusClient import *
+import time
 
 modbusclient = ModbusClient('127.0.0.1', 502)
 modbusclient.connect()
 
 # holdingRegisters = modbusclient.read_holdingregisters(0,5)
-holdingRegisters = modbusclient.write_single_register(0,22)
+# holdingRegisters = modbusclient.write_single_register(0,22)
+
+for awef in range(0,20) : 
+    holdingRegisters = modbusclient.write_single_register(0,awef)
+    holdingRegisters = modbusclient.write_single_register(1,awef+10)
+    time.sleep(1)
 
 
 print(holdingRegisters)
