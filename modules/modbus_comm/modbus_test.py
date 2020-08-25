8f313f6bb702114a01bbc51b672591efd5e4bd94
qqq000feel <qqq000feel@naver.com>
2020-08-25 14:19:36 +0900

    20200825-modbus-test

--- modules/modbus_comm/modbus_test.py
+++ modules/modbus_comm/modbus_test.py
@@ -5,7 +5,7 @@ modbusclient = ModbusClient('127.0.0.1', 502)
 modbusclient.connect()
 
 # holdingRegisters = modbusclient.read_holdingregisters(0,5)
-holdingRegisters = modbusclient.write_single_register(0,6721)
+holdingRegisters = modbusclient.write_single_register(0,22)
 
 
 print(holdingRegisters)
