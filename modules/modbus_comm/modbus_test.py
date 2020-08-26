54b8676d4ecc0176ee6dc84d2c61a2349ece878c
qqq000feel <qqq000feel@naver.com>
2020-08-26 14:06:16 +0900

    20200826

--- modules/modbus_comm/modbus_test.py
+++ modules/modbus_comm/modbus_test.py
@@ -1,45 +1,27 @@
 from easymodbus.modbusClient import ModbusClient
 from easymodbus.modbusClient import *
-import time
 
+import time
 import sys
+import threading
+
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtGui import *
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QMessageBox
 
 import time
 import datetime
 from datetime import timedelta
-import copy
 
+import urllib
 from urllib.request import urlopen
 from urllib.parse import urlencode, unquote, quote_plus
 
-import urllib
+import copy
 import json
 import csv
-
-# import threading
-
-
-
-# modbusclient = ModbusClient('127.0.0.1', 502)
-# modbusclient.connect()
-
-# # holdingRegisters = modbusclient.read_holdingregisters(0,5)
-# # holdingRegisters = modbusclient.write_single_register(0,22)
-
-# for awef in range(0,20) : 
-#     holdingRegisters = modbusclient.write_single_register(0,awef)
-#     holdingRegisters = modbusclient.write_single_register(1,awef+10)
-#     time.sleep(1)
-
-
-# print(holdingRegisters)
-
-# modbusclient.close()
-
 
 form_class = uic.loadUiType("C:\\Users\\ECODA\\Desktop\\dhwtest\\pyapitest\\BASS-BEMS\\modules\\modbus_comm\\modbus_test.ui")[0]
 
