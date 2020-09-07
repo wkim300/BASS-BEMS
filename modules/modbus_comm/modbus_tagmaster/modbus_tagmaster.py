481b53af1300e114d1c46eed0ab3e188048e77b2
qqq000feel <qqq000feel@naver.com>
2020-09-07 14:23:39 +0900

    20200907

--- modules/modbus_comm/modbus_tagmaster/modbus_tagmaster.py
+++ modules/modbus_comm/modbus_tagmaster/modbus_tagmaster.py
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import QMessageBox
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtCore import QThread
 
-form_class = uic.loadUiType("treewidgettest.ui")[0]
+form_class = uic.loadUiType("modbus_tagmaster.ui")[0]
 
 class WindowClass(QMainWindow, form_class) :
 
